2c20b033729140afadd5108296bb635739797fba
atree <atree.public@gmail.com>
2022-10-30 23:15:51 +0900

    Get AC in problem B

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use proconio::{fastout, input, marker::Usize1};
+
+const M: usize = 26;
+
+#[fastout]
 fn main() {
-    println!("Hello, world!");
+    input! {
+        d: usize,
+        c: [i32; M],
+        s: [[i32; M]; d],
+        t: [Usize1; d]
+    }
+    let mut score = 0;
+    let mut last = vec![0; M];
+    for i in 0..d {
+        score += s[i][t[i]];
+        last[t[i]] = i + 1;
+        for j in 0..M {
+            score -= c[j] * (i as i32 + 1 - last[j] as i32);
+        }
+        println!("{}", score);
+    }
 }
